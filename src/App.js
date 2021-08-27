fbe179f2de241a872ad320f6cb83e8c02ca43358
이기석 <lightandliberty@naver.com>
2021-08-27 23:15:38 +0900

    71p까지 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 //import Potato from './Potato';  // ./는 같은 경로. App.js와 Potato.js는 같은 폴더이므로.
 
-function Potato(){
-  <h3>I love Potato</h3>;
+function Food(){
+  return <h1>I love Potato</h1>;
 }
 
 function App() {
-  return (<div><h1>Hello</h1><Potato /></div>);
+  return (
+    <div>
+       <h1>Hello</h1>
+       <Food fav = "kimchi" />
+    </div>
+  );
 }
 
 export default App;
