36f06fa4da3620054d36a8ac9a559a1ec10036ba
이기석 <lightandliberty@naver.com>
2021-08-27 18:28:07 +0900

    Potato.js파일 생성 후, 그냥 App.js에 Potato()를 만듦

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+//import Potato from './Potato';  // ./는 같은 경로. App.js와 Potato.js는 같은 폴더이므로.
+
+function Potato(){
+  <h3>I love Potato</h3>;
+}
 
 function App() {
-  return (<div><h1>Hello</h1></div>);
+  return (<div><h1>Hello</h1><Potato /></div>);
 }
 
 export default App;
