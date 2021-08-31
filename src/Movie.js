ccbbe41eb67db89783507c2b869e6ea557390726
이기석 <lightandliberty@naver.com>
2021-08-31 23:14:18 +0900

    App.js, Movie.js에 HTML추가. 포스터 표시 완료

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 function Movie({id, title, year, summary, poster}){
-    return <h4>{title}</h4>; // title만 우선 출력
+    return (
+        <div class = "movie">
+        <img src={poster} alt={title} title={title} />
+
+        <div class = "movie__data">
+            <h3 class="movie__title">{title}</h3>
+            <h5 class="movie__year">{year}</h5>
+            <p class = "movie__summary">{summary}</p>
+        </div>
+        </div>
+    );
 }
 
+// 매개변수 유효성 검사
 // 데이터를 확인 후, id를 Movie.propTypes에 추가
 Movie.propTypes = {
      id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired, };  // poster는 이미지의 주소를 저장
+    poster: PropTypes.string.isRequired,
+ };  // poster는 이미지의 주소를 저장
 
 export default Movie;
