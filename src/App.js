ba4b5505373d997a5e1e740d12cff5e97f47564e
이기석 <lightandliberty@naver.com>
2021-08-31 10:37:37 +0900

    135p 영화 앱 만들기 워밍업. 이전 생명주기함수 다 지움

--- src/App.js
+++ src/App.js
@@ -1,62 +1,10 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 
-class App extends React.Component{
-  state = {
-    count: 0,
-  };
-  
-  // 생성될 때 호출
-  constructor(props)
-  {
-    super(props);
-    console.log('constructed. I\'m constructor()');
-  }  
-  
+class App export React.Component{
   render(){
-    console.log('now rendering. I\'m rendering()');
-     return (
-     <div>
-        <h1>The number is: {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-     </div>
-      );
-   }
-   // render() -> componentDidMount() 실행 됨.
-  componentDidMount(){
-    console.log('component rendered I\'m componentDidMount()');
-  }  
-  
-  // render() -> componentDidUpdate() 실행 됨.
-  componentDidUpdate()
-  {
-    console.log("I just updated. I'm componentDidUpdate()");
-  }  
-  
-  componentWillUnmount()
-  {
-    console.log("Destructed. I'm componentWillUnmount");
+    return <div/>;
   }
-
-  add = () => {
-    console.log('add');
-    this.setState(current => (
-      {
-        count: current.count + 1,   // { index: current.count -1 } 객체를 반환
-      }
-    ));  // 새로운 state {count: 1} 을 인자로 넘겨 줌.
-  }
-  
-  minus = () => {
-    console.log('minus');
-    this.setState(current => (
-      {
-        count: current.count -1,  // { index: current.count -1 } 객체를 반환
-      }
-    )); // 새로운 state {count: -1} 을 인자로 넘겨 줌.
-  }
-
-
 }
 
 export default App;
